50f981e9986ee1685b77d73ebc6c8b7cb1612158
Iduranga Heshan Dissanayake <78864748+idurangaheshan@users.noreply.github.com>
2021-12-26 01:44:30 +0530

    name

--- reactwebsite/src/App.js
+++ reactwebsite/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Iduranga Heshan Dissanayake
+          Iduranga Heshan Dissanayaka
         </p>
         <a
           className="App-link"
